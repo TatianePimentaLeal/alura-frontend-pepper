feffd990d0f1bb26af5b98ff04d54010e9781a09
TatyPimentaLeal <tatyplummer07@gmail.com>
2023-11-20 07:25:16 -0300

    Finalizando o projeto

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -4,20 +4,55 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const mudarIcone = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer');
+
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioBeepTempoFinalizado = new Audio('/sons/beep.mp3');
+const audioPlay = new Audio('/sons/play.wav');
+const audioPause = new Audio('/sons/pause.mp3')
+
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    mostrarTempo()
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
+
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -36,3 +71,52 @@ function alterarContexto(contexto) {
             break;
     }
 }
+
+//Função de decremento
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0){
+        audioBeepTempoFinalizado.play()
+        alert('Tempo finalizado!')
+        zerar()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        audioPause.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = "Pausar"
+    mudarIconeParaPausa()
+}
+
+function zerar(){
+    clearInterval(intervaloId)
+    iniciarOuPausarBt.textContent = "Começar"
+    mudarIconeParaPlay()
+    intervaloId = null
+}
+
+function mudarIconeParaPausa() {
+	mudarIcone.src = "/imagens/pause.png";
+}
+
+function mudarIconeParaPlay() {
+    mudarIcone.src = "/imagens/play_arrow.png"
+}
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo() //mostrando nol escopo global
